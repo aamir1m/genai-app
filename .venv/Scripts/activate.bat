b651d4a35c2a3f82b76d1a6f85f726e678c92396
amogra123 <aamir.mogra@capgemini.com>
2024-08-14 14:55:23 +0100

    remove venv from repo

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\aamir\code\genai-app\.venv
+set VIRTUAL_ENV=c:\Users\amogra\python-random\genai-app\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
